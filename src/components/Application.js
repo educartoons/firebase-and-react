4ef549418e846466fe63e6a999f6f5c0315f313c
educartoons <educartoons@gmail.com>
2020-01-28 09:01:17 -0500

    Using useContext

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,18 +7,12 @@ import Authentication from './Authentication';
 
 class Application extends Component {
   state = {
-    posts: [],
     user: null,
   };
 
-  unsubscribeFromFirestore = null;
   unsubscribeFromAuth = null;
 
   componentDidMount = async () =>{
-    this.unsubscribeFromFirestore = firestore.collection('posts').onSnapshot(snapshot=>{
-      const posts = snapshot.docs.map(collectIdsAndDocs);
-      this.setState({posts})
-    });
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth=>{
       const user = await createUserProfileDocument(userAuth);
@@ -33,13 +27,13 @@ class Application extends Component {
   }
 
   render() {
-    const { posts, user } = this.state;
+    const {user } = this.state;
 
     return (
       <main className="Application">
         <h1>Think Piece</h1>
         <Authentication user={user}/>
-        <Posts posts={posts} />
+        <Posts />
       </main>
     );
   }
