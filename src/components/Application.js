dfe57f563ee220a1f6f020d562d5aa5c75b702b6
educartoons <educartoons@gmail.com>
2020-01-28 09:19:09 -0500

    Adding User Context

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,33 +6,11 @@ import {collectIdsAndDocs} from '../utils/utilities';
 import Authentication from './Authentication';
 
 class Application extends Component {
-  state = {
-    user: null,
-  };
-
-  unsubscribeFromAuth = null;
-
-  componentDidMount = async () =>{
-
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth=>{
-      const user = await createUserProfileDocument(userAuth);
-      console.log(user)
-      this.setState({user})
-    })
-
-  }
-
-  componentWillUnmount = () => {
-    this.unsubscribeFromFirestore();
-  }
-
   render() {
-    const {user } = this.state;
-
     return (
       <main className="Application">
         <h1>Think Piece</h1>
-        <Authentication user={user}/>
+        <Authentication />
         <Posts />
       </main>
     );
--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import './index.scss';
 
 import Application from './components/Application';
 import PostsProvider from './components/providers/PostsProvider'
+import UserProvider from './components/providers/UserProvider';
 
 render(
-  <PostsProvider>
-    <Application />,
-  </PostsProvider> 
+  <UserProvider>
+    <PostsProvider>
+      <Application />
+    </PostsProvider>
+  </UserProvider>
   ,document.getElementById('root'));
