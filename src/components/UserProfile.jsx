256d9b1193de3699c4c857ff17205e3dc6e666a1
educartoons <educartoons@gmail.com>
2020-01-28 20:00:51 -0500

    added UserProfile update functions

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,8 +1,41 @@
 import React, {Component} from 'react';
+import { auth, firestore } from '../firebase';
 
 class UserProfile extends Component{
+  state = {
+    displayName: ''
+  }
+  imageInput = null;
+  get uid(){
+    return auth.currentUser.uid;
+  }
+  get userRef(){
+    return firestore.doc(`/users/${this.uid}`);
+  }
+  handleChange = event => {
+    const {name, value} = event.target;
+    this.setState({
+      [name]: value
+    })
+  }
+  handleSubmit = event => {
+    event.preventDefault();
+    const {displayName} = this.state;
+    if(displayName){
+      this.userRef.update({displayName});
+    }
+  }
   render(){
-    return <div>This is totally a user profile page</div>
+    const {displayName} = this.state;
+    return (
+      <section className="UserProfile">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="displayName" value={displayName} placeholder="Display Name" onChange={this.handleChange}/>
+          <input type="file" ref={ref=>this.imageInput = ref}/>
+          <input className="update" type="submit"/>
+        </form>
+      </section>
+    )
   }
 }
 
